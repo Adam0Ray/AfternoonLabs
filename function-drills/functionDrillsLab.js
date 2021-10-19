1326536f9987ce025c214bcd038f0c0f367d73a5
Adam Ray <aray58@gmail.com>
2021-10-19 16:55:48 -0500

    Object-orientation Lab Initial Commit

--- function-drills/functionDrillsLab.js
+++ function-drills/functionDrillsLab.js
@@ -314,7 +314,7 @@
 let sampleArray = [0,1,2,3,4,7,5,6,8,9]
 function checkArrayAscending(arr) {
   let unsorted = JSON.stringify(arr)  // returns [0,1,2,3,4,7,5,6,8,9]
-  let sorted = JSON.stringify(arr.sort(function(a,b){return a-b })) //returns [0,1,2,3,4,5,6,7,8,9]
+  let sorted = JSON.stringify(arr.sort(function(a,b){return a-b })) //returns [0,1,2,3,4,5,6,7,8,9] sorting using logic of browser
   if(sorted === unsorted){
       console.log('true')
   } else {console.log('false')}
