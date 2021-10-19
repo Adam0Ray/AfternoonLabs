ddcd12e3c056d9807eed76ce5bfd1e042a5a44a8
Adam Ray <aray58@gmail.com>
2021-10-19 08:25:40 -0500

    JSON added to problem 15

--- function-drills/functionDrillsLab.js
+++ function-drills/functionDrillsLab.js
@@ -295,21 +295,34 @@
 */
 
 // CODE HERE
+// let sampleArray = [0,1,2,3,4,7,5,6,8,9]
+// // let sampleArray = [0,1,2,3,4,5,6,7,8,9]
+// function check(arr){
+//   let comparisonValue = arr[0]
+//   for (let i = 1; i < arr.length-1; i++){
+//   if (arr[i] <= comparisonValue) {
+//     return false
+//   } else {
+//     comparisonValue = arr[i]
+//   }
+// }
+// return true
+// }
+
+// let arrayIsAscending = console.log(check(sampleArray))
+
 let sampleArray = [0,1,2,3,4,7,5,6,8,9]
-// let sampleArray = [0,1,2,3,4,5,6,7,8,9]
-function check(arr){
-  let comparisonValue = arr[0]
-  for (let i = 1; i < arr.length-1; i++){
-  if (arr[i] <= comparisonValue) {
-    return false
-  } else {
-    comparisonValue = arr[i]
-  }
-}
-return true
+function checkArrayAscending(arr) {
+  let unsorted = JSON.stringify(arr)  // returns [0,1,2,3,4,7,5,6,8,9]
+  let sorted = JSON.stringify(arr.sort(function(a,b){return a-b })) //returns [0,1,2,3,4,5,6,7,8,9]
+  if(sorted === unsorted){
+      console.log('true')
+  } else {console.log('false')}
 }
 
-let arrayIsAscending = console.log(check(sampleArray))
+checkArrayAscending(sampleArray)
+// console.log( JSON.stringify(sampleArray))
+// console.log( JSON.stringify(sampleArray.sort(function(a,b){return a-b })))
 
 ////////////////// PROBLEM 16 ////////////////////
 
